2241ba08694d8c66814a953f669e00f58320a87b
miguel flores ramirez <miguelf1671@gmail.com>
2023-08-30 00:42:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const allCats = [...cats, name];
+    return allCats
+}
+
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats
+}
+
+function removeLastCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats
+}
